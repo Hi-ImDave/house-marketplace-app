feb058bf87da3cea6e38248b5d557354882711a2
David Mott <d.s.mott0808@gmail.com>
2022-10-25 10:02:40 -0700

    edit listing func

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -102,6 +102,8 @@ const Profile = () => {
     }
   }
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return (
     <div className='profile'>
       <header className='profileHeader'>
@@ -160,6 +162,7 @@ const Profile = () => {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
